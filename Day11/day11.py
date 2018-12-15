5bf31be6a730021710e09b9cbde2552fe2bbcf6e
Erik Söderberg <Zednosius@gmail.com>
2018-12-15 13:01:20 +0100

    Add working part 2, is a bit faster than previously

--- Day11/day11.py
+++ Day11/day11.py
@@ -1,6 +1,6 @@
 import numpy as np
 from math import sin, cos, radians
-from operator import sub
+from operator import sub, add
 
 GS = 300
 
@@ -58,32 +58,38 @@ def calculate_xbyx_slice(grid, size):
 
 
 def calculate_all_sizes(grid):
-    cache = {}
-    sum_grid = np.zeros((GS, GS, GS))
-    for size in range(GS):
-        print(size)
-        for x in range(GS-size):
-            for y in range(GS-size):
-                # sum_grid[x, y, size] = np.sum(grid[x:x+size, y:y+size])
-                # sum_grid[x, y, size] += sum_slice(grid, x, y, size)
-                sum_grid[x, y, size] += np.sum(grid[x+size-1, y:y+size])+np.sum(grid[x:x+size-1, y+size-1])
-                if size > 0:
-                    sum_grid[x, y, size] += sum_grid[x, y, size-1]
-        # cache[size] = calculate_xbyx_slice(grid, size, cache.get(size-1, None))
-    # print(cache[16][81, 268])
-    print(sum_grid[89, 268, 16])
-    print(sum_grid[90, 269, 16])
-    return cache
+    sum_grid = np.zeros((GS, GS, GS), dtype=int)
+    sum_grid[:, :, 0] += grid
+    for size in range(1, GS):
+        print(size, end='\r')
+
+        xmask, ymask = np.indices((GS-size, GS-size))  # for size=1, which is a 2x2 the last index is 298
+        sum_grid[:, :, size] = sum_grid[:, :, size-1]
+        for x in range(size+1):
+            sum_grid[xmask, ymask, size] += grid[xmask+x, ymask+size]
+
+        for y in range(size):  # Or we sum x+size,y+size twice
+            sum_grid[xmask, ymask, size] += grid[xmask+size, ymask+y]
+
+        # for x in range(GS-size):
+        #     for y in range(GS-size):
+        #         np.testing.assert_equal((sum_grid[x, y, size]), np.sum(grid[x:x+size+1, y:y+size+1]))
+    print()
+    return sum_grid
 
 
 if __name__ == '__main__':
     # test()
-    # grid = main(7165)
-    grid = main(18)
+    grid = main(7165)
+    # grid = main(18)
     print(grid)
     sums = calculate_3by3(grid)
     print(sums)
     max_index = np.unravel_index(np.argmax(sums), (GS, GS))
     print("Max index: ", max_index)  # Is the top left corner of 3by3
     print("Max value: ", sums[max_index])
-    calculate_all_sizes(grid)
+
+    sum_grid = calculate_all_sizes(grid)
+    max_index = np.unravel_index(np.argmax(sum_grid), (GS, GS, GS))
+    print("Max index: ", tuple(map(add, max_index, (1, 1, 1))))
+    print("Max value: ", sum_grid[max_index])
